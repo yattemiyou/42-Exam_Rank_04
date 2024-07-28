6b33ccad2b896c69ac300b3ad57b02dbab87b4ad
Your Name <you@example.com>
2024-07-28 11:22:08 +0900

    テスト

--- test.sh
+++ test.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-echo "/*** 課題 ***/"
+echo "/*** 課題（引数なし） ***/"
+./microshell
+echo $?
+
+echo "/*** 課題（引数あり） ***/"
 ./microshell /bin/ls "|" /usr/bin/grep microshell ";" /bin/echo i love my microshell
+echo $?
 
 echo "/*** 3連cat ***/"
 ./microshell /bin/cat "|" /bin/cat "|" /bin/cat
+echo $?
 
 echo "/*** 2連cat + ls ***/"
 ./microshell /bin/cat "|" /bin/cat "|" /bin/ls
+echo $?
